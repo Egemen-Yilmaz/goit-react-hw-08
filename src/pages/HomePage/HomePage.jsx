33a53fa11199640b9b448ab788c47fe9b71b0e35
egemen <egemenylmz01@gmail.com>
2026-02-06 14:33:29 +0300

    home fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import styles from './HomePage.module.css';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { selectIsLoggedIn, selectUser } from '../../redux/auth/selectors';
 
 const HomePage = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const user = useSelector(selectUser);
+
   return (
     <section className={styles.container}>
       <h1>Welcome to Contacts App</h1>
-      <p>Use the navigation to register or login.</p>
+      {isLoggedIn ? (
+        <>
+          <p>Hello{user && user.name ? `, ${user.name}` : ''}! You're signed in.</p>
+          <p>
+            Go to your <Link to="/contacts">contacts</Link>.
+          </p>
+        </>
+      ) : (
+        <p>Use the navigation to register or login.</p>
+      )}
     </section>
   );
 };
